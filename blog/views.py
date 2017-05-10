7a7e21f9cb258ec4ddaeafd8f752eb354c9bc51d
--replace-all <nathanbowness@gmail.com>
2017-05-10 16:22:00 +0000

    Changed some of the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Very simple view to be seen, accessed by urls.py which calls post_list
+def post_list(request):
+    # post_list.html will be accessed by this
+    return render(request, 'blog/post_list.html', {})
+
+
