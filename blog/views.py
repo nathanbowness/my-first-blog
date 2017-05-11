5f838c11b749c1e8cfbf0221aba42a1400ca8c7e
--replace-all <nathanbowness@gmail.com>
2017-05-11 13:59:52 +0000

    Added the blog posts to main page

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 # Very simple view to be seen, accessed by urls.py which calls post_list
 def post_list(request):
     # post_list.html will be accessed by this
-    return render(request, 'blog/post_list.html', {})
 
-
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
