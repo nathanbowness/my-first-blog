c25df2b3678968654960bb8dc59564a5b49bbe59
--replace-all <nathanbowness@gmail.com>
2017-05-12 11:52:38 +0000

    Added in post details

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    # The P<pk> will take everything you place here and transfer it to a view as a variable called pk
+    #If say /post/5 was done, it would transfer the information that pk 5
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail')
 ]
